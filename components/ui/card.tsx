d75dbd2e3979e3acd5b0e8e7aeb6f303333ae53f
roannogueira84-ui <roan.nogueira84@gmail.com>
2025-08-29 16:25:21 -0300

    Create card.tsx

---
+++ components/ui/card.tsx
@@ -0,0 +1,20 @@
+import * as React from "react";
+import { cn } from "./utils";
+
+export function Card({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) {
+  return <div className={cn("rounded-lg border bg-white shadow-sm", className)} {...props} />;
+}
+export function CardHeader({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) {
+  return <div className={cn("p-4 border-b", className)} {...props} />;
+}
+export function CardContent({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) {
+  return <div className={cn("p-4", className)} {...props} />;
+}
+export function CardFooter({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) {
+  return <div className={cn("p-4 border-t", className)} {...props} />;
+}
+
+// helper simples
+export function cn(...classes: Array<string | undefined | false>) {
+  return classes.filter(Boolean).join(" ");
+}
