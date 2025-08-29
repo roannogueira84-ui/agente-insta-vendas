ccc7d8eb54f6fcd2bed424ea1bac965a6028f0d4
roannogueira84-ui <roan.nogueira84@gmail.com>
2025-08-29 18:42:52 -0300

    Update db.ts

--- lib/db.ts
+++ lib/db.ts
@@ -1,13 +1,12 @@
+// lib/db.ts
 import { PrismaClient } from "@prisma/client";
 
-const globalForPrisma = globalThis as unknown as { prisma?: PrismaClient };
+const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
 export const prisma =
-  globalForPrisma.prisma ??
+  globalForPrisma.prisma ||
   new PrismaClient({
-    log: process.env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"]
+    log: ["error", "warn"],
   });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
-
-export default prisma;
