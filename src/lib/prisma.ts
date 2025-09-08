ad016020d34ee10625d4ec897239ae5e7c162475
roannogueira84-ui <roan.nogueira84@gmail.com>
2025-09-08 19:16:25 -0300

    Update prisma.ts

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -7,7 +7,7 @@ const globalForPrisma = globalThis as unknown as { prisma?: PrismaClient };
 export const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
-    // log: ["query"], // opcional p/ debug
+    // log: ["query"], // opcional
   });
 
 if (process.env.NODE_ENV !== "production") {
